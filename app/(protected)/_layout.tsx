5f137c34517ac390a055576ca913dffa7eee779d
harold-ramirez <ramirezpereiraharold@gmail.com>
2025-10-14 09:58:31 -0400

    feat: :sparkles: Enhance ProtectedLayout with websocket connection and push notifications; update Login component layout and styles

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,21 +1,31 @@
+import { registerForPushNotifications } from "@/services/notificationService";
+import SocketService from "@/services/socketService";
 import { AuthContext } from "@/utils/authContext";
 import { Redirect, Stack } from "expo-router";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
-export default function RootLayout() {
+export { ErrorBoundary } from "expo-router";
+export const unstable_settings = {
+  initialRouteName: "(tabs)",
+};
+
+export default function ProtectedLayout() {
   const authState = useContext(AuthContext);
-  if (!authState) {
-    return <Redirect href={"/login"} />;
-  }
 
-  // // Websocket connection
-  // useEffect(() => {
-  //   registerForPushNotifications();
-  //   SocketService.connect();
-  //   return () => {
-  //     SocketService.disconnect();
-  //   };
-  // }, []);
+  // Websocket connection
+  useEffect(() => {
+    if (authState?.isLoggedIn) {
+      registerForPushNotifications();
+      SocketService.connect();
+    }
+    return () => {
+      SocketService.disconnect();
+    };
+  }, [authState?.isLoggedIn]);
+
+  if (!authState.isLoggedIn) {
+    return <Redirect href={"../login"} />;
+  }
 
   return (
     <Stack
--- app/login.tsx
+++ app/login.tsx
@@ -6,13 +6,13 @@ import {
 } from "@/components/Icons";
 import { AuthContext } from "@/utils/authContext";
 import { LinearGradient } from "expo-linear-gradient";
-// import { useRouter } from "expo-router";
 import { useContext, useState } from "react";
 import {
   Image,
   KeyboardAvoidingView,
   Platform,
   Pressable,
+  ScrollView,
   StatusBar,
   Text,
   TextInput,
@@ -20,11 +20,6 @@ import {
 } from "react-native";
 
 export default function Login() {
-  // const router = useRouter();
-
-  // const handleLogin = () => {
-  //   router.replace("/(protected)/(tabs)");
-  // };
   const [peekPassword, setPeekPassword] = useState(false);
   const [formData, setFormData] = useState({
     user: "",
@@ -35,83 +30,106 @@ export default function Login() {
 
   return (
     <>
-      <StatusBar barStyle={"light-content"} />
+      <StatusBar barStyle={"dark-content"} />
+      <LinearGradient
+        colors={["#02457A", "#018ABE"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        className="absolute top-0 bottom-0 right-0 left-0"
+      />
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
-        className="absolute justify-center w-full h-full"
+        className="absolute items-center w-full h-full"
       >
-        <LinearGradient
-          colors={["#001B48", "#018ABE", "#001B48"]}
-          className="top-0 right-0 left-0 absolute h-full"
-        />
-
-        <View
-          style={{ marginTop: -72 }}
-          className="justify-center items-center"
+        <ScrollView
+          className="w-full h-full flex-1"
+          showsVerticalScrollIndicator={false}
         >
-          <Image
-            source={require("../assets/images/logo.png")}
-            style={{ width: 200, height: 200, marginBottom: 48 }}
-            className="rounded-full"
-          />
-
-          <LinearGradient
-            colors={["rgba(255, 255, 255, 0.1)", "rgba(255, 255, 255, 0.3)"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            className="items-center gap-5 blur-xl p-5 border border-whiteBlue/30 rounded-xl w-4/5"
-            style={{
-              borderRadius: 12,
-            }}
-          >
-            <Text className="font-bold text-white text-4xl">Bienvenido</Text>
-
-            <View className="flex-row items-center gap-1 bg-whiteBlue rounded-lg h-14">
-              <ProfileIconAlt color="#001B48" size={21} className="pl-2" />
-              <TextInput
-                className="flex-1"
-                placeholder="Ingrese su Usuario..."
-                value={formData.user}
-                onChangeText={(text) =>
-                  setFormData({ ...formData, user: text })
-                }
-              />
-            </View>
-            <View className="flex-row items-center gap-1 bg-whiteBlue rounded-lg h-14">
-              <LockIcon color="#001B48" size={28} className="pl-2" />
-              <TextInput
-                className="flex-1"
-                placeholder="Ingrese su Contraseña..."
-                secureTextEntry={!peekPassword}
-                value={formData.password}
-                onChangeText={(text) =>
-                  setFormData({ ...formData, password: text })
-                }
-              />
-              <Pressable
-                className="justify-center items-center p-1 h-full"
-                onPress={() => setPeekPassword(!peekPassword)}
-              >
-                {peekPassword ? (
-                  <EyeIcon color="#999999" size={16} />
-                ) : (
-                  <EyeSlashIcon color="#999999" size={16} />
-                )}
-              </Pressable>
-            </View>
-
-            <Pressable
-              onPress={authContext.logIn}
-              android_ripple={{ color: "#018ABE" }}
-              className="bg-blackBlue mt-5 px-5 py-2 border border-whiteBlue rounded-lg"
+          <View className="w-full h-full flex-1 items-center justify-center">
+            {/* Image Banner */}
+            <View
+              className="items-center bg-whiteBlue p-5"
+              style={{
+                width: "96%",
+                borderTopWidth: 0,
+                borderRadius: 30,
+                borderTopEndRadius: 0,
+                borderTopLeftRadius: 0,
+              }}
             >
-              <Text className="font-semibold text-whiteBlue text-lg">
-                Iniciar Sesión
+              <Image
+                source={require("../assets/images/adaptive-icon.png")}
+                style={{ width: 300, height: 200 }}
+              />
+            </View>
+
+            <View className="gap-10 pt-16">
+              <Text className="font-bold text-whiteBlue text-4xl">
+                Mi Consultorio Dental
               </Text>
-            </Pressable>
-          </LinearGradient>
-        </View>
+              <LinearGradient
+                colors={["#FFFFFF1A", "#FFFFFF4D"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                className="items-center gap-5 blur-xl p-5 border border-whiteBlue/30 rounded-xl w-4/5"
+                style={{
+                  borderRadius: 12,
+                }}
+              >
+                <Text className="font-bold text-white text-4xl">
+                  Bienvenido
+                </Text>
+                <View className="flex-row items-center gap-1 bg-whiteBlue rounded-lg h-14">
+                  <ProfileIconAlt color="#001B48" size={21} className="pl-2" />
+                  <TextInput
+                    className="flex-1 text-blackBlue"
+                    placeholder="Ingrese su Usuario..."
+                    placeholderTextColor={"gray"}
+                    value={formData.user}
+                    onChangeText={(text) =>
+                      setFormData({ ...formData, user: text })
+                    }
+                  />
+                </View>
+                <View className="flex-row items-center gap-1 bg-whiteBlue rounded-lg h-14">
+                  <LockIcon color="#001B48" size={28} className="pl-2" />
+                  <TextInput
+                    className="flex-1 text-blackBlue"
+                    placeholder="Ingrese su Contraseña..."
+                    placeholderTextColor={"gray"}
+                    secureTextEntry={!peekPassword}
+                    value={formData.password}
+                    onChangeText={(text) =>
+                      setFormData({ ...formData, password: text })
+                    }
+                  />
+                  <Pressable
+                    className="justify-center items-center p-1 h-full"
+                    onPress={() => setPeekPassword(!peekPassword)}
+                  >
+                    {peekPassword ? (
+                      <EyeIcon color="#999999" size={16} />
+                    ) : (
+                      <EyeSlashIcon color="#999999" size={16} />
+                    )}
+                  </Pressable>
+                </View>
+
+                {/* Login Button */}
+                <Pressable
+                  onPress={authContext.logIn}
+                  android_ripple={{ color: "#018ABE" }}
+                  className="bg-blackBlue mt-5 px-5 py-2 border border-whiteBlue rounded-lg"
+                >
+                  <Text className="font-semibold text-whiteBlue text-lg">
+                    Iniciar Sesión
+                  </Text>
+                </Pressable>
+              </LinearGradient>
+            </View>
+          </View>
+        </ScrollView>
       </KeyboardAvoidingView>
     </>
   );
